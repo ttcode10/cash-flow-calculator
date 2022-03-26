18182dfd3e81576e3959fdfd52b89a0d92b41acd
ttcode10 <ttcode10@gmail.com>
2022-03-26 20:07:49 +1100

    get results from firestore

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,29 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { collection, doc, getDocs, setDoc } from '@firebase/firestore';
 import styled from 'styled-components';
 
-import { Background, Button, Card, FormControl, Heading, Input } from '../shared/components';
+import { db } from '../firebase-config';
+import { Background, Button, Card, FormControl, H2Text, H6Text, Input } from '../shared/components';
 import { colors, spacing } from '../shared/themes';
 
 const Slogan = styled.div`
   margin-bottom: ${spacing.spacing8x};
-  text-align: left;
 `;
 
-export const Homepage = () => (
-  <Background>
-    <Slogan>
-      <Heading styleLevel={2} color={colors.white}>
-        A good property can sustain itself.
-      </Heading>
-    </Slogan>
-    <Card>
-      <FormControl label="Selling Price">
-        <Input startEnhancer="$" onChange={() => console.log('sdfsdf')} />
-      </FormControl>
-      <FormControl label="Weekly Rent">
-        <Input startEnhancer="$" onChange={() => console.log('sdfsdf')} />
-      </FormControl>
-      <Button onClick={() => console.log('sdfsdf')}>Submit</Button>
-    </Card>
-  </Background>
-);
+interface SheetProps {
+  sellingPrice?: number;
+  weeklyRent?: number;
+}
+
+interface ResultProps {
+  sellingPrice?: number;
+  weeklyRent?: number;
+}
+
+export const Homepage = () => {
+  const [sheet, setSheet] = useState<SheetProps>();
+  const sheetsCollectionRef = collection(db, 'sheets');
+  const [result, setResult] = useState<ResultProps>();
+  const [sheetId, setSheetId] = useState('');
+  const [isSubmitButtonDisabled, setIsSubmitButtonDisabled] = useState(false);
+
+  const onInputChange = (event) => {
+    const newValue = {
+      ...sheet,
+      [event.target.name]: event.target.value,
+    };
+    setSheet(newValue);
+  };
+
+  const generateNewCaseId = () => {
+    const id = Math.floor(new Date().valueOf() * Math.random())
+      .toString(36)
+      .toUpperCase();
+    setSheetId(id);
+  };
+
+  const getResult = async () => {
+    const data = await getDocs(sheetsCollectionRef);
+    return data.docs.find((item) => item.id === sheetId)?.data();
+  };
+
+  const addSheet = async () => {
+    setIsSubmitButtonDisabled(true);
+    await setDoc(doc(sheetsCollectionRef, sheetId), sheet);
+    const calculationResult = await getResult();
+    if (calculationResult) {
+      setResult(calculationResult);
+      setIsSubmitButtonDisabled(false);
+    }
+  };
+
+  useEffect(() => {
+    generateNewCaseId();
+  }, []);
+
+  return (
+    <Background>
+      <Slogan>
+        <H2Text color={colors.white}>A good property can sustain itself.</H2Text>
+      </Slogan>
+      <Card>
+        <FormControl label="Selling Price">
+          <Input startEnhancer="$" onChange={onInputChange} name="sellingPrice" />
+        </FormControl>
+        <FormControl label="Weekly Rent">
+          <Input startEnhancer="$" onChange={onInputChange} name="weeklyRent" />
+        </FormControl>
+        <Button
+          onClick={addSheet}
+          isLoading={isSubmitButtonDisabled}
+          disabled={isSubmitButtonDisabled}
+        >
+          Submit
+        </Button>
+        <H6Text>Selling Price: {result?.sellingPrice}</H6Text>
+        <H6Text>Weekly Rent: {result?.weeklyRent}</H6Text>
+      </Card>
+    </Background>
+  );
+};
--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -4,8 +4,12 @@ import { Button as BaseButton } from 'baseui/button';
 interface ButtonProps {
   onClick: () => void;
   children: string;
+  isLoading: boolean;
+  disabled: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ onClick, children }) => (
-  <BaseButton onClick={onClick}>{children}</BaseButton>
+export const Button: React.FC<ButtonProps> = ({ onClick, children, isLoading, disabled }) => (
+  <BaseButton onClick={onClick} disabled={disabled} isLoading={isLoading}>
+    {children}
+  </BaseButton>
 );
